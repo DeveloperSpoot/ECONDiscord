b73199e5fc35aca6658fb013ca42eb166fda074e
Nathaniel Rivera <riveranathaniel173@gmail.com>
2025-07-26 16:37:14 -0700

    Updated Discord server info. Easter eeg.

--- utils/PromotionUtil.js
+++ utils/PromotionUtil.js
@@ -2,7 +2,7 @@ const {EmbedBuilder} = require("discord.js");
 
 exports.currentPromotion = async function() {
 
-    let promoOps = [['https://i.imgur.com/ei1wDfp.png', '[Join Our Server](https://discord.gg/8BdC9VfguF)'],['https://i.imgur.com/CS7qDTN.png', '[Get Access To Premium Features](https://discord.com/application-directory/1077139728538812416/store/1260839276069785653)'], ['https://i.imgur.com/WZxz4r5.png', '[Get Access To Premium Features](https://discord.com/application-directory/1077139728538812416/store/1260839276069785653)'], ['https://i.imgur.com/mmrwlJu.png', '[Vote On Top.gg](https://top.gg/bot/1077139728538812416/vote)']]
+    let promoOps = [['https://i.imgur.com/ei1wDfp.png', '[Join Our Server](https://discord.gg/tgg2cyYHDh)'],['https://i.imgur.com/CS7qDTN.png', '[Get Access To Premium Features](https://discord.com/application-directory/1077139728538812416/store/1260839276069785653)'], ['https://i.imgur.com/WZxz4r5.png', '[Get Access To Premium Features](https://discord.com/application-directory/1077139728538812416/store/1260839276069785653)']]
 
 //Update
     const random = Math.floor(Math.random() * promoOps.length);
@@ -10,6 +10,6 @@ exports.currentPromotion = async function() {
         .setDescription(promoOps[random][1])
         .setColor('#2B2D31')
         .setImage(promoOps[random][0])
-        .setFooter({text: "Servers that support us don't see these promotions."});
+        .setFooter({text: "Are we back? | Servers that support us don't see these promotions."});
 
 }
