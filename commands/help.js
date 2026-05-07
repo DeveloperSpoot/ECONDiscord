f2321ee57ed9edfb7a17fed42eeed118433f0b31
Nathaniel Rivera <52226518+DeveloperSpoot@users.noreply.github.com>
2026-05-07 07:07:16 +0000

    Updated help command, removed old information.

--- commands/help.js
+++ commands/help.js
@@ -17,7 +17,7 @@ module.exports = {
             .setDescription("In order to use any command, you must first be registered. To register run </register:1077339153194303531> command. Need help setting up ECON? Run the </setup:1260740541771939920> command.")
             .setColor("#33E300")
             .setTimestamp()
-            .setFooter({text: "ECON by Spectacle Development"})
+            .setFooter({text: "ECON by DevSpoot"})
             .addFields(
                 { name: 'Treasury', value: '/treasury', inline: true },
                 { name: 'Business', value: '/bus', inline: true },
@@ -25,7 +25,6 @@ module.exports = {
                 { name: 'Support Us', value: '[Discord Premium](https://discord.com/application-directory/1077139728538812416/store/1260839276069785653)', inline: true },
                 { name: 'Bot Guide', value: '[View all bot commands here.](https://econ.spectacledev.com/Documentation)', inline: true },
                 {name: 'Support Server', value: '[Join The Discord Server](https://discord.gg/tgg2cyYHDh)', inline: true},
-                {name: 'Legal', value: '[Terms of Service](https://spectacledev.com/econ-termsofservice) | [Privacy Policy](https://spectacledev.com/econ-privacypolicy)', inline: true},
                 {name: 'Vote For The Bot', value: '[Vote on Top.gg](https://top.gg/bot/1077139728538812416)', inline: true},
                 {name: 'Invite The Bot', value: '[Invite Link](https://discord.com/api/oauth2/authorize?client_id=1077139728538812416&permissions=414464723008&scope=bot)', inline: true},
                 {name: 'Forms', value: '[Report A Bug](https://discord.gg/tgg2cyYHDh) | [Make A Suggestion](https://discord.gg/tgg2cyYHDh)', inline: true}
