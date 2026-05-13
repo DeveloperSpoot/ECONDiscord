6412b211cf140b7bd1cfcf644d1b5b665d692e4b
Nathaniel Rivera <52226518+DeveloperSpoot@users.noreply.github.com>
2026-05-13 20:38:44 +0000

    I think it's time for ECON 2.0.

--- dataCrusher/services/department.js
+++ dataCrusher/services/department.js
@@ -543,7 +543,7 @@ Department.prototype = {
                 }
             });
 
-        if (foundBind && await foundBind?.permissions.find(v=> v === "Department-Head")){
+        if (await foundBind?.permissions.find(v=> v === "Department-Head")){
             violaton ="The Department Head Role cannot be removed. This role may not be edited through role-bind commands."
               throw new Error(violaton)
         }
