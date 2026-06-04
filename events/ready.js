caebc7cd4fd582fc4fc5dedf387357dcac2922e8
saatvikk <saatvikrk@pm.me>
2026-06-04 03:18:59 +0100

    forex channel

--- events/ready.js
+++ events/ready.js
@@ -1,7 +1,7 @@
 const { scheduleForexUpdates } = require('../dataCrusher/services/forexUpdate');
 
 module.exports = {
-	name: 'ready',
+	name: 'clientReady',
 	once: true,
 	execute(client) {
 		console.log(`Ready! Logged in as ${client.user.tag}`);
